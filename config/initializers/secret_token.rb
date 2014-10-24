866902a58954c0106b4201683b520ba745ffaaff
Hugo Angeles <hugoangeles0810@gmail.com>
2014-10-23 22:08:45 -0500

    Creacion de proyecto RailsNote

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsNote::Application.config.secret_key_base = '014c8e3c02837384bc5fdd98d23986b7a69f6858809424cef72e61e502329994f60ad6a99e52871daa10ecdde09a9401dc70b33de2d62c4130bd9abeb8e9fd1a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsNote::Application.config.session_store :cookie_store, key: '_RailsNote_session'
