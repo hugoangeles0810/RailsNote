bf8683e5aaf29abc5a12ddb63aaaa7a99223f478
Hugo Angeles <hugoangeles0810@gmail.com>
2014-10-24 21:57:42 -0500

    Agregando Bootstrap3 y formulario login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
-  def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  protected
+	  def current_user
+	  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	  end
+
+	  def authenticate
+	  	current_user.is_a? User || access_denied
+	  end
+
+	  def access_denied
+	  	redirect_to log_in_path, notice: "Please log in to continue"
+	  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RailsNote::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
+  get "sessions" => "sessions#new"
 
   get "sign_up" => "users#new", :as => "sign_up"
   root :to => "users#new"
